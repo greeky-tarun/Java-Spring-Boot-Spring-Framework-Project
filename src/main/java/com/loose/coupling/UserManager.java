d3d46541ed8c5a55f766148eaf05edd5b30f0b8a
B. Tarun Kumar <btarunkumar7010@gmail.com>
2025-11-24 23:22:52 +0530

    Loose Coupling in Java

---
+++ src/main/java/com/loose/coupling/UserManager.java
@@ -0,0 +1,8 @@
+package com.loose.coupling;
+
+public class UserManager {
+    private UserDatabase userDatabase = new UserDatabase();
+    public String getUserInfo(){
+        return userDatabase.getUserDetails();
+    }
+}
